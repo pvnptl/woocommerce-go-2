afdfd939c419eb169040b328c824a405c7f70b66
Pavan Patil <pavapatil@gmail.com>
2024-10-23 04:51:49 +0530

    feat: Canges

--- entity/product_variation.go
+++ entity/product_variation.go
@@ -1,9 +1,5 @@
 package entity
 
-import (
-	"time"
-)
-
 // ProductVariationAttribute product variation attribute properties
 type ProductVariationAttribute struct {
 	ID     int    `json:"id"`
@@ -14,20 +10,20 @@ type ProductVariationAttribute struct {
 // ProductVariation product variation properties
 type ProductVariation struct {
 	ID                int               `json:"id"`
-	DateCreate        time.Time         `json:"date_create,omitempty"`
-	DateCreateGMT     time.Time         `json:"date_create_gmt,omitempty"`
-	DateModified      time.Time         `json:"date_modified,omitempty"`
-	DateModifiedGMT   time.Time         `json:"date_modified_gmt,omitempty"`
+	DateCreate        string            `json:"date_create,omitempty"`
+	DateCreateGMT     string            `json:"date_create_gmt,omitempty"`
+	DateModified      string            `json:"date_modified,omitempty"`
+	DateModifiedGMT   string            `json:"date_modified_gmt,omitempty"`
 	Description       string            `json:"description"`
 	Permalink         string            `json:"permalink"`
 	SKU               string            `json:"sku"`
 	Price             float64           `json:"price"`
 	RegularPrice      float64           `json:"regular_price"`
 	SalePrice         float64           `json:"sale_price"`
-	DateOnSaleFrom    time.Time         `json:"date_on_sale_from"`
-	DateOnSaleFromGMT time.Time         `json:"date_on_sale_from_gmt"`
-	DateOnSaleTo      time.Time         `json:"date_on_sale_to"`
-	DateOnSaleToGMT   time.Time         `json:"date_on_sale_to_gmt"`
+	DateOnSaleFrom    string            `json:"date_on_sale_from"`
+	DateOnSaleFromGMT string            `json:"date_on_sale_from_gmt"`
+	DateOnSaleTo      string            `json:"date_on_sale_to"`
+	DateOnSaleToGMT   string            `json:"date_on_sale_to_gmt"`
 	OnSale            bool              `json:"on_sale"`
 	Status            string            `json:"status"`
 	Purchasable       bool              `json:"purchasable"`
